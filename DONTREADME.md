8310af9946cdf1503a4fbfa06cbce9d98a272cce
Nadav Tasger <tashernadav@gmail.com>
2019-11-09 22:43:58 +0200

    Progress - Functional

--- DONTREADME.md
+++ DONTREADME.md
@@ -6,3 +6,12 @@
 2. `BC` - n'th and n'th+1 characters of `foobar`, AES encrypted with clientKey+hash
 3. `D` - n, AES encrypted with clientKey + appSigHash
 4. `E` - Key part checksum
+
+# 2
+let appname be F00Bar?!
+let block be F00Bar?!F00Bar?!
+let encrypted be AES(block)
+let license be ABCD-ABCD-ABCD-ABCD-ABCD-ABCD-ABCD-ABCD
+let A be nth character of appname
+let BC be nth pair of bytes of encrypted
+let D be sum(ABC)^n%256
--- android/app/src/main/java/com/kipodafterfree/f00bar/app/KeyValidator.java
+++ android/app/src/main/java/com/kipodafterfree/f00bar/app/KeyValidator.java
@@ -6,67 +6,56 @@ import android.util.Base64;
 import android.util.Log;
 
 import com.kipodafterfree.f00bar.R;
-import com.kipodafterfree.f00bar.game.PopupUtil;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.KeySpec;
-import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class KeyValidator {
 
     private Activity activity;
-    private String keuehpxcmm, epocelubwh, yomtxbjgnh;
+    private String keuehpxcmm, epocelubwh, yomtxbjgnh, nsjtajrhfg;
 
     public KeyValidator(Activity activity) throws NoSuchAlgorithmException, PackageManager.NameNotFoundException, IOException {
         this.activity = activity;
         this.yomtxbjgnh = new PreferenceManager(activity).modrnzzhxp();
         this.keuehpxcmm = new AppIntegrityGuard(activity).sbvoxfhuul();
         this.epocelubwh = new AppIntegrityGuard(activity).zsquncjvzt();
+        this.nsjtajrhfg = activity.getResources().getString(R.string.app_name) + activity.getResources().getString(R.string.app_name);
     }
 
-    public boolean dlujefokcl(String key) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
-        String clientKey = keuehpxcmm + yomtxbjgnh;
-        String appSigKey = epocelubwh + yomtxbjgnh;
-        String name = activity.getResources().getString(R.string.app_name);
-        Log.i("Sig", epocelubwh);
-        Log.i("Has", keuehpxcmm);
-        Log.i("Cli", yomtxbjgnh);
+    public boolean dlujefokcl(String key) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException {
+        Log.i("Hash", keuehpxcmm);
         String[] split = key.split("-");
-        Log.i("len", split.length+"");
-        if (split.length != 6)
+        if (split.length != 8)
             return false;
-        for (int part = 0; part < split.length; part++) {
-            Log.i("Hi", "HIII");
-            byte[] kpart = Base64.decode(split[part], Base64.DEFAULT);
-//            if (kpart.length != 33)
-//                return false;
-//            if (kpart[0] != name.charAt(part))
-//                return false;
-            byte[] foobarEncrypted = new byte[16];
-            System.arraycopy(kpart, 1, foobarEncrypted, 0, foobarEncrypted.length);
-            Log.i("foobar", "foo "+new String(decrypt(qqnyqbrmoq(clientKey).getBytes(), foobarEncrypted)));
-            if (!Arrays.equals(decrypt(qqnyqbrmoq(clientKey).getBytes(), foobarEncrypted), name.substring(part, part + 2).getBytes()))
+        byte[] wholeEncrypted = new byte[16];
+        for (int n = 0; n < split.length; n++) {
+            byte[] part = Base64.decode(split[n], Base64.DEFAULT);
+            if (part.length != 4)
                 return false;
-            byte[] partEncrypted = new byte[16];
-            System.arraycopy(kpart, 1 + foobarEncrypted.length, partEncrypted, 0, partEncrypted.length);
-            if (!Arrays.equals(decrypt(qqnyqbrmoq(appSigKey).getBytes(), partEncrypted), String.valueOf(part).getBytes()))
+            byte a = part[0];
+            byte b = part[1];
+            byte c = part[2];
+            byte d = part[3];
+            if (a != nsjtajrhfg.charAt(n))
                 return false;
-
+            if ((byte) Math.pow(a + b + c, n + 1) != d)
+                return false;
+            wholeEncrypted[n * 2] = b;
+            wholeEncrypted[n * 2 + 1] = c;
         }
-        return true;
+        String decrypted = new String(fdsnjfesnj(qqnyqbrmoq(keuehpxcmm.trim() + epocelubwh.trim() + yomtxbjgnh.trim()).getBytes(StandardCharsets.UTF_8), wholeEncrypted), StandardCharsets.UTF_8);
+        return decrypted.equals(nsjtajrhfg);
     }
 
     private String qqnyqbrmoq(String s) {
@@ -77,17 +66,10 @@ public class KeyValidator {
         return generated;
     }
 
-    private static byte[] encrypt(byte[] raw, byte[] clear) throws Exception {
-        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-        return cipher.doFinal(clear);
-    }
-
-    private static byte[] decrypt(byte[] raw, byte[] encrypted) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+    private byte[] fdsnjfesnj(byte[] raw, byte[] encrypted) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+        SecretKeySpec key = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(nsjtajrhfg.getBytes(StandardCharsets.UTF_8)));
         return cipher.doFinal(encrypted);
     }
 
