6110db9924a4a5776dd8e76810c08146b166611c
Nadav Tasger <tashernadav@gmail.com>
2019-11-10 17:52:40 +0200

    Build script

--- build.php
+++ build.php
@@ -9,6 +9,11 @@ const APACHE_CRT_FILE = CERTIFICATE_DIRECTORY . DIRECTORY_SEPARATOR . "out.crt";
 const APP_CRT_FILE = APP_RAW_DIRECTORY . DIRECTORY_SEPARATOR . "certificate.crt";
 const APP_JSON_FILE = APP_RAW_DIRECTORY . DIRECTORY_SEPARATOR . "app.json";
 
+const APP_KEYSTORE = __DIR__ . DIRECTORY_SEPARATOR . "android" . DIRECTORY_SEPARATOR . "keystore.jks";
+const APP_APK = APP_DIRECTORY . DIRECTORY_SEPARATOR . "release" . DIRECTORY_SEPARATOR . "app-release.apk";
+
+const WEB_APK = __DIR__ . DIRECTORY_SEPARATOR . "web" . DIRECTORY_SEPARATOR . "files" . DIRECTORY_SEPARATOR . "public" . DIRECTORY_SEPARATOR . "foobar.apk";
+
 echo "We'll need you to enter some information:\n";
 $domain = readline("Domain Name: ");
 $port = readline("Port Number: ");
@@ -22,6 +27,10 @@ $object = new stdClass();
 $object->domain = $domain;
 $object->port = $port;
 file_put_contents(APP_JSON_FILE, json_encode($object));
-echo "Compiling app";
-shell_exec(APP_DIRECTORY . DIRECTORY_SEPARATOR . "gradlew assemble");
-echo "Signing app";
+echo "Compiling & signing app";
+shell_exec(APP_DIRECTORY . DIRECTORY_SEPARATOR . "gradlew assembleRelease -Pandroid.injected.signing.store.file=" . APP_KEYSTORE . " -Pandroid.injected.signing.store.password=12345678 -Pandroid.injected.signing.key.alias=slicense -Pandroid.injected.signing.key.password=12345678");
+echo "Copying APK";
+copy(APP_APK, WEB_APK);
+echo "Building Docker";
+shell_exec("docker build " . __DIR__ . " -t slicense");
+echo "All done.";
